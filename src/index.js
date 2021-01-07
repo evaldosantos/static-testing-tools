88a8cde6ee5aa2c2b3884316385a4308f244c1fb
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-07 20:23:36 -0300

    setup eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!')
+const username = 'freddy'
+typeof username === 'strng'
+
+if (!'serviceWorker' in navigator) {
+  // you have an old browser
+}
+
+const greeting = 'hello'
+console.log('${greeting} world!');
+[1,2,3].forEach(x => console.log(x))
